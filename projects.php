4503fd2d42fcd160306d700b048bad01da0f2935
Banabanaa <imbmillosa@gmail.com>
2025-04-22 03:59:47 +0800

    Added Different Showcases for projects

--- projects.php
+++ projects.php
@@ -38,89 +38,117 @@
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="project1.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/.jpg" 
+                title="CvSU Enrollment System" data-description="An enrollment system designed for CvSU-Bacoor City Campus" 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="cvsuES.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-2.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/chess.png" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-2.jpg" title="Gallery 2" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/chess.png" 
+                title="CvSU Chess Club" data-description="A Chess App that is a fully functional game application built using Java." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="chessclub.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-3.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/cvsu.png" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-3.jpg" title="Gallery 3" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/cvsu.png" 
+                title="CvSU Website" data-description="A simple website where it showcases the mission, vision, courses offered, and organizations of the school." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="cvsuwebsite.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-4.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/lebug1.png" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-4.jpg" title="Gallery 4" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/lebug1.png" 
+                title="LE BUG" data-description="A simple portfolio website designed for a small IT company to effectively showcase its expertise and services." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="lebug.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-5.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-5.jpg" title="Gallery 5" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/" 
+                title="CvSU Voting System" data-description="A web voting system designed for the Commission on Elections Organization of the CvSU Bacoor Campus." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="votingsystem.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-6.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/emes.png" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-6.jpg" title="Gallery 6" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/emes.png" 
+                title="EMES LIT Galing" data-description="The system was developed to create an effective educational website for Grade 6 students at Ezra Marie Elementary School." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="emes.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-7.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/pos.png" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-7.jpg" title="Gallery 7" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/pos.png" 
+                title="POS System" data-description="A user-friendly POS application for the owners of Likhang Kamay to easily access their inventory and sales tracking." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="POSsystem.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
 
           <div class="col-xl-3 col-lg-4 col-md-6">
             <div class="gallery-item h-100">
-              <img src="assets/img/gallery/gallery-8-2.jpg" class="img-fluid" alt="">
+              <img src="assets/img/projects/" class="img-fluid" alt="">
               <div class="gallery-links d-flex align-items-center justify-content-center">
-                <a href="assets/img/gallery/gallery-8-2.jpg" title="Gallery 8" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                <a href="gallery-single.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+                <a href="assets/img/projects/" 
+                title="COG Checklist" data-description="A simple web Checklist that shows student's grades throughout the years." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="checklist.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
               </div>
             </div>
-          </div><!-- End Gallery Item -->
+          </div>
+
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div class="gallery-item h-100">
+              <img src="assets/img/projects/chess1.png" class="img-fluid" alt="">
+              <div class="gallery-links d-flex align-items-center justify-content-center">
+                <a href="assets/img/projects/chess1.png" 
+                title="YOU DO NOTE" data-description="A simple note-taking app built with Django." 
+                class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+                <a href="youdonote.php" class="details-link"><i class="bi bi-link-45deg"></i></a>
+              </div>
+            </div>
+          </div>
 
         </div>
 
       </div>
 
-    </section><!-- /Gallery Section -->
+    </section>
 
   </main>
 
